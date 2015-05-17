4393ede46775427680a6d48279c2a1bafc18cbf5
Kamille Delgardo <kamilledelgardo@gmail.com>
2015-05-16 20:33:40 -0700

    minor changes as I get sleepier and more violent

--- Greeting.js
+++ Greeting.js
@@ -24,17 +24,17 @@ return greeting + ", honey! The time is " + watchTime + ".";
 var dayOfWeek = d.getDay();
 var TGIFMessage;
 if (dayOfWeek === 0 || dayOfWeek === 6) {
-  TGIFMessage = "What the good-God-damn are you doing out of bed on the weekend?!";
+  TGIFMessage = "What the good-God-damn are you doing \nOUT\nOF\nBED\non the weekend?!";
 }
 else if (dayOfWeek >= 1 || dayOfWeek <= 3) {
-  TGIFMessage = "Only " + (5 - dayOfWeek) + "more days until Friday!";
+  TGIFMessage = "Only " + (5 - dayOfWeek) + "more days until Friday!\nYour hair looks great today, by the way.";
 }
 else if (dayOfWeek === 4) {
   TGIFMessage = "Tomorrow's Friday! You're almost there...";
 }
 else {
-  TGIFMessage = "TGIF, BITCHES.";
+  TGIFMessage = "TGIF, BITCHES.\nYou deserve a drank.";
 }
-
+//console.logging until I figure out fucking HTML goddamnit shit fuck
 console.log(heyHoney());
 console.log(TGIFMessage);
