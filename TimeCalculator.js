9067de85cc511067507200ad0e2fe1db200d6045
Kamille <kamilledelgardo@gmail.com>
2015-06-14 19:37:06 +0200

    Will this work?

---
+++ TimeCalculator.js
@@ -0,0 +1,17 @@
+var minTime = 4; //one minute each way up and down the escalators, grabbing drink, paying, through the turnstiles, etc
+//var d = new Date();
+//var watchTime = d.toLocaleTimeString();
+
+var bartTime = 12; //MUST CHANGE TO API THING
+
+//list of drinks in order of how long they take to prepare, +1 minute for each.
+var coffeeDranks = ["coffee", "iced coffee", "cafe au lait", "cafe latte", "pumpkin spiced latte"];
+
+
+function drankList(arr) {
+  
+}
+
+
+console.log("The next train to Berkeley is coming in " + bartTime + " minutes. ");
+//document.write("You have time for: " +
