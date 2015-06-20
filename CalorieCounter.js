e6d370fa2fe0f5517e4f5fa36637b934d24efbe6
Kamille <kamilledelgardo@gmail.com>
2015-06-20 23:02:35 +0200

    minor fixes, mostly bouncing around with css and making the calorie counter more user friendly

--- CalorieCounter.js
+++ CalorieCounter.js
@@ -4,31 +4,29 @@ function calCalculator(){
       var height = document.getElementById("height").value;
       var weight = document.getElementById("weight").value;
       var sex = document.getElementById("sex").value;
-      
-      //turn inputs into usable data
-      age = parseInt(age, 10);
-      height = parseInt(height, 10);
-      weight = parseInt(weight, 10);
-      
-      //declare values for following equation
-      var recCalories;
-      var percentage;
-      var calsInPSL = 375; //there are 375 calories in a large whole milk Peet's Pumpkin Spice Latte
+      if  (age === "" || weight === "" || height === "" || sex === "") {
+        document.getElementById("output").innerHTML= "Missing factor!";
+      }
+      else {
+        //turn inputs into usable data
+        age = parseInt(age, 10);
+        height = parseInt(height, 10);
+        weight = parseInt(weight, 10);
         
-        if (sex === 'F' || sex === 'f') {
-          recCalories = Math.floor(1.375 * ((447.593 + (9.247 * weight) + (3.098 * height)) - (4.330 * age)));
-        }
-        
-        else if (sex === 'M' || sex === 'm') {
-          recCalories = Math.floor(1.375 * ((88.362 + (13.397 * weight) + (4.799 * height)) - (5.677 * age)));
-        }
-        
-        percentage = Math.floor(100 * (calsInPSL / recCalories));
-        document.getElementById("output").innerHTML= 'You just ate ' + percentage + '% of your daily recommended ' + recCalories + ' calories!';
-        
-        output = function() {
-          if (event.keyCode == 13) {
-          alert('You just ate ' + percentage + '% of your daily recommended ' + recCalories + ' calories!');
+        //declare values for following equation
+        var recCalories;
+        var percentage;
+        var calsInPSL = 375; //there are 375 calories in a large whole milk Peet's Pumpkin Spice Latte
+          
+          if (sex === 'F' || sex === 'f') {
+            recCalories = Math.floor(1.375 * ((447.593 + (9.247 * weight) + (3.098 * height)) - (4.330 * age)));
           }
-        };
+          
+          else if (sex === 'M' || sex === 'm') {
+            recCalories = Math.floor(1.375 * ((88.362 + (13.397 * weight) + (4.799 * height)) - (5.677 * age)));
+          }
+          
+          percentage = Math.floor(100 * (calsInPSL / recCalories));
+          document.getElementById("output").innerHTML= 'You just ate ' + percentage + '% of your daily recommended ' + recCalories + ' calories!';
+        } 
       }
