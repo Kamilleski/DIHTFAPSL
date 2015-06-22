92488f3b80be571e0cbbc685b4dd3d148c613e23
Kamille <kamilledelgardo@gmail.com>
2015-06-22 13:22:32 +0200

    Changed all files to better align with airbnb style guide as described at https://github.com/airbnb/javascript

--- Greeting.js
+++ Greeting.js
@@ -9,12 +9,15 @@ var heyHoney = function() {
   if (hour < 12 && hour >= 0) {
     greeting = 'Good morning' ;
   }
+  
   else if (hour <= 18) {
     greeting = 'Good afternoon';
   }
+  
   else {
     greeting = 'Good evening';
   }
+  
 return greeting + ', honey! The time is ' + watchTime + '.';
 };
 
@@ -22,14 +25,17 @@ return greeting + ', honey! The time is ' + watchTime + '.';
 var dayOfWeek = d.getDay();
 var TGIFMessage;
 if (dayOfWeek === 0 || dayOfWeek === 6) {
-  TGIFMessage = 'What the good-God-damn are you doing OUT OF BED on the weekend?!';
+  TGIFMessage = 'What the hell are you doing OUT OF BED on the weekend?!';
 }
+
 else if (dayOfWeek >= 1 && dayOfWeek <= 3) {
-  TGIFMessage = 'Only ' + (5 - dayOfWeek) + ' more days until Friday... Your hair looks great today, by the way!';
+  TGIFMessage = 'Only ' + (5 - dayOfWeek) + ' more days until Friday... ' + 
+  'Your hair looks great today, by the way!';
 }
 else if (dayOfWeek === 4) {
-  TGIFMessage = "Tomorrow's Friday! You're almost there...";
+  TGIFMessage = "Tomorrow is Friday! You're almost there...";
 }
+
 else {
   TGIFMessage = 'TGIF, BITCHES. You deserve a drank.';
 }
@@ -38,4 +44,5 @@ else {
 function totalGreeting(salutation, body) {
   document.write(salutation + "  " + body);
 }
+
 totalGreeting(heyHoney(), TGIFMessage);
--- TimeCalculator.js
+++ TimeCalculator.js
@@ -1,9 +1,11 @@
 
-//one minute each way up and down the escalators, grabbing drink, paying, through the turnstiles, etc
+/*one minute each way up and down the escalators, grabbing drink, paying, 
+through the turnstiles, etc*/
 var minTime = 4; 
 
 //list of drinks in order of how long they take to prepare, +1 minute for each.
-var coffeeDranks = ['a coffee', 'an iced coffee', 'a cafe au lait', 'a cafe latte', 'a pumpkin spice latte'];
+var coffeeDranks = ['a coffee', 'an iced coffee', 'a cafe au lait', 
+  'a cafe latte', 'a pumpkin spice latte'];
 
 //creating a string with the list of what drinks you could get based on the times
 function drankList(arr, mins) {
@@ -11,16 +13,20 @@ function drankList(arr, mins) {
       var stringy = arr.join(', or ');
       return 'you have time to grab ' + stringy + '!';
   }
+  
   if (mins <= 3) {
     return 'Nonono! You barely have time to make it, let alone get a drink! Run, girl!';
   }
+  
   else if (mins >= 9) {
     return 'YES, ' + drankString();
   }
+  
   else {
     while ((arr.length) >= mins) {
       arr.pop();
     }
+    
     return "Sadly you don't have time for a PSL today, but " + drankString();
   }
 }
@@ -33,28 +39,45 @@ xhr.onreadystatechange = function() {
   var mins;
   xmlDoc=xhr.responseXML;
   if (xhr.readyState==4 && xhr.status==200){
-    var k = xmlDoc.getElementsByTagName("etd");   //creates an array of etd objects
-    for (var i = 0; i <k.length; i++) {   //looping to use only the train lines that lead to Richmond
-      var abbr = (k[i].getElementsByTagName("abbreviation")[0].childNodes[0].nodeValue);
+    //creates array of etd objects
+    var k = xmlDoc.getElementsByTagName("etd");   
+    //looping to use only the train lines that lead to Richmond
+    for (var i = 0; i <k.length; i++) {   
+      var abbr = (k[i].getElementsByTagName("abbreviation")[0].childNodes[0].
+        nodeValue);
       if (abbr === "PITT") {
         mins = k[i].getElementsByTagName("minutes")[0].childNodes[0].nodeValue;
         mins = parseInt(mins);
-        /*departTimes.push(mins);
+        /** FIXME: Richmond line should be included by pushing its data into an
+         * array along with the other data and then sorting it.  Right now it's
+         * only using Pittsburg/BayPoint data.  Preliminary try below:
+        departTimes.push(mins);
         departTimes.sort(function(a, b){
           return a-b;
           });
-        mins = departTimes[0]*/
-        if (mins <= 2) { //if number of minutes is less than time it takes to even run between trains, move to next viable etd
-          negativeMessage = " You couldn't make the next train even if you ran, so take your time!";
+        mins = departTimes[0]
+        */
+        
+        /**if number of minutes is less than time it takes to even run between 
+         * trains, move to next viable estimated time of departure
+         */
+        if (mins <= 2) { 
+          negativeMessage = " You couldn't make the next train even if you" + 
+            " ran, so take your time!";
           mins = k[i].getElementsByTagName("minutes")[1].childNodes[0].nodeValue;
           mins = parseInt(mins); 
-          document.getElementById("runTime").innerHTML= drankList(coffeeDranks, mins) + negativeMessage + " The next viable train is leaving in " + mins + " minutes.";
+          document.getElementById("runTime").innerHTML= drankList(coffeeDranks, 
+            mins) + negativeMessage + " The next viable train is leaving in " + 
+            mins + " minutes.";
         }
+        
         else
-          document.getElementById("runTime").innerHTML= drankList(coffeeDranks, mins) + ' The next train is leaving in ' + mins + ' minutes.';
+          document.getElementById("runTime").innerHTML= drankList(coffeeDranks, 
+            mins) + ' The next train is leaving in ' + mins + ' minutes.';
         } 
       }
     }
+    
   return mins;
 };
 
